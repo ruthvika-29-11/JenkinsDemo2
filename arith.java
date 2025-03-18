355f73402275236a722be317ca9b8709668e4112
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-03-18 12:41:00 +0530

    Update arith.java

--- arith.java
+++ arith.java
@@ -4,9 +4,9 @@ class arith
 	public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter 1st value:");
-	int a=sc.nextInt();
+	int a=10;
 	 System.out.println("Enter 2nd value:");
-        int b=sc.nextInt();
+        int b=5;
 	    System.out.println("Sum:"+(a+b));
 	    System.out.println("Difference:"+(a-b));
 	    System.out.println("Product:"+(a*b));
