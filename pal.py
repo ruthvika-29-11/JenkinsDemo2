3be4b9fbc62e03859b458ef27ab2416544525d0a
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-03-18 13:06:23 +0530

    Fc

---
+++ pal.py
@@ -0,0 +1,13 @@
+s="Hello"
+p="abba"
+if s[::]==s[::-1]:
+
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+if p[::]==p[::-1]:
+
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
