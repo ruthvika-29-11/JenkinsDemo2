30ca87d9fad58145dedd5fe7959457790ff4227b
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-03-24 12:36:32 +0530

    Sc

---
+++ CommandLineDemo2.java
@@ -0,0 +1,12 @@
+CommanLineDemo2
+{
+public static void main(String[] args)
+{
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+System.out.println("Hi This is: "+s1);
+System.out.println("I am from: "+s2);
+System.out.println("My roll no is: "+s3);
+}
+}
